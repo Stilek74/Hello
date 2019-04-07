1884f0eafc0df4ba163d7968771e8b1024d35df6
Adam Bojar <adambojar@gmail.com>
2019-04-08 00:09:49 +0200

    git add -A

---
+++ Hello/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("Tutaj wpisz swoje imię: ");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("Witaj, " + name);
+
+                    Console.Write("Teraz podaj nam swój wiek: ");
+                }
+                int age;
+                bool result = int.TryParse(Console.ReadLine(), out age);
+
+                if (age > 17)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("Brawo, możesz do nas dołączyć!");
+                }
+                else if (result == false)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Przykro nam, nie wprowadziłeś poprawnego wieku");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("Przykro nam, nie możesz do nas dołączyć");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
