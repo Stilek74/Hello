3760cd50bbdb4d49b4726eba690c594aecd27eec
Adam Bojar <adambojar@gmail.com>
2019-04-08 18:40:43 +0200

    git add diary git commit

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -10,36 +10,70 @@ namespace Hello
     {
         static void Main(string[] args)
         {
+            ValueTypes();
+
             for (; ; )
             {
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write("Tutaj wpisz swoje imię: ");
-                    string name = Console.ReadLine();
-                    Console.WriteLine("Witaj, " + name);
-
-                    Console.Write("Teraz podaj nam swój wiek: ");
-                }
-                int age;
-                bool result = int.TryParse(Console.ReadLine(), out age);
-
-                if (age > 17)
-                {
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine("Brawo, możesz do nas dołączyć!");
-                }
-                else if (result == false)
-                {
-                    Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.WriteLine("Przykro nam, nie wprowadziłeś poprawnego wieku");
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Magenta;
-                    Console.WriteLine("Przykro nam, nie możesz do nas dołączyć");
-                }
-                Console.ReadKey();
+                Powitanie();
+                Wiek();
+                Settings();
             }
+        }
+
+        private static void ValueTypes()
+        {
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            long maxLong = long.MaxValue;
+            long minLong = long.MinValue;
+
+            Console.WriteLine("maxInt = " + maxInt);
+            Console.WriteLine("minInt = " + minInt);
+            Console.WriteLine("maxLong = " + maxLong);
+            Console.WriteLine("minLong = " + minLong);
+        }
+
+        private static void Settings()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
+        }
+
+        private static void Wiek()
+        {
+            {
+
+                Console.Write("Teraz podaj nam swój wiek: ");
+            }
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+            if (age > 17)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Brawo, możesz do nas dołączyć!");
+            }
+            else if (result == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Przykro nam, nie wprowadziłeś poprawnego wieku");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Przykro nam, nie możesz do nas dołączyć");
+            }
+        }
+        /// <summary>
+        /// Wypisujemy powitanie użytkownika
+        /// </summary>
+        private static void Powitanie()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Tutaj wpisz swoje imię: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Witaj, " + name);
         }
     }
 }
---
+++ StudentDiary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDiary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();
+            diary.AddRating(5);
+            diary.AddRating(1);
+            diary.AddRating(3);
+
+            float Avg = diary.CalculateAverage();
+            float Max = diary.GiveMaxRating();
+            float Min = diary.GiveMinRating();
+
+        }
+    }
+}
